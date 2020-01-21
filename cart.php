8cde47887d0ddae49d5318f8317f7df8eee609cb
Ouweshs28 <ouweshseeroo@gmail.com>
2020-01-21 13:20:35 +0400

    Mirnal Code

---
+++ cart.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Media Store</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+   
+   <div id="top"><!-- Top Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="col-md-6 offer"><!-- col-md-6 offer Begin -->
+               
+               <a href="#" class="btn btn-success btn-sm">Welcome</a>
+               <a href="checkout.php">4 Items In Your Cart | Total Price: $300 </a>
+               
+           </div><!-- col-md-6 offer Finish -->
+           
+           <div class="col-md-6"><!-- col-md-6 Begin -->
+               
+               <ul class="menu"><!-- cmenu Begin -->
+                   
+                   <li>
+                       <a href="register.php">Sign up</a>
+                   </li>
+                   <li>
+                       <a href="checkout.php">My Account</a>
+                   </li>
+                   <li>
+                       <a href="cart.php">Go To Cart</a>
+                   </li>
+                   <li>
+                       <a href="checkout.php">Login</a>
+                   </li>
+                   
+               </ul><!-- menu Finish -->
+               
+           </div><!-- col-md-6 Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- Top Finish -->
+   
+   <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="navbar-header"><!-- navbar-header Begin -->
+               
+               <a href="index.php" class="navbar-brand home"><!-- navbar-brand home Begin -->
+                   
+                   <img src="" alt="Media Store Logo" class="hidden-xs">
+                   <img src="" alt="Media Store Logo Mobile" class="visible-xs">
+                   
+               </a><!-- navbar-brand home Finish -->
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                   
+                   <span class="sr-only">Toggle Navigation</span>
+                   
+                   <i class="fa fa-align-justify"></i>
+                   
+               </button>
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                   
+                   <span class="sr-only">Toggle Search</span>
+                   
+                   <i class="fa fa-search"></i>
+                   
+               </button>
+               
+           </div><!-- navbar-header Finish -->
+           
+           <div class="navbar-collapse collapse" id="navigation"><!-- navbar-collapse collapse Begin -->
+               
+               <div class="padding-nav"><!-- padding-nav Begin -->
+                   
+                   <ul class="nav navbar-nav left"><!-- nav navbar-nav left Begin -->
+                       
+                       <li>
+                           <a href="index.php">Home</a>
+                       </li>
+                       <li>
+                           <a href="shop.php">Shop</a>
+                       </li>
+                       <li>
+                           <a href="checkout.php">My Account</a>
+                       </li>
+                       <li class="active">
+                           <a href="cart.php">Shopping Cart</a>
+                       </li>
+                       <li>
+                           <a href="contact.php">Contact Us</a>
+                       </li>
+                       
+                   </ul><!-- nav navbar-nav left Finish -->
+                   
+               </div><!-- padding-nav Finish -->
+               
+               <a href="cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary Begin -->
+                   
+                   <i class="fa fa-shopping-cart"></i>
+                   
+                   <span>4 Items In Your Cart</span>
+                   
+               </a><!-- btn navbar-btn btn-primary Finish -->
+               
+               <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right Begin -->
+                   
+                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn Begin -->
+                       
+                       <span class="sr-only">Toggle Search</span>
+                       
+                       <i class="fa fa-search"></i>
+                       
+                   </button><!-- btn btn-primary navbar-btn Finish -->
+                   
+               </div><!-- navbar-collapse collapse right Finish -->
+               
+               <div class="collapse clearfix" id="search"><!-- collapse clearfix Begin -->
+                   
+                   <form method="get" action="results.php" class="navbar-form"><!-- navbar-form Begin -->
+                       
+                       <div class="input-group"><!-- input-group Begin -->
+                           
+                           <input type="text" class="form-control" placeholder="Search" name="user_query" required>
+                           
+                           <span class="input-group-btn"><!-- input-group-btn Begin -->
+                           
+                           <button type="submit" name="search" value="Search" class="btn btn-primary"><!-- btn btn-primary Begin -->
+                               
+                               <i class="fa fa-search"></i>
+                               
+                           </button><!-- btn btn-primary Finish -->
+                           
+                           </span><!-- input-group-btn Finish -->
+                           
+                       </div><!-- input-group Finish -->
+                       
+                   </form><!-- navbar-form Finish -->
+                   
+               </div><!-- collapse clearfix Finish -->
+               
+           </div><!-- navbar-collapse collapse Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- navbar navbar-default Finish -->
+   
+   <div id="content"><!-- #content Begin -->
+       <div class="container"><!-- container Begin -->
+           <div class="col-md-12"><!-- col-md-12 Begin -->
+               
+               <ul class="breadcrumb"><!-- breadcrumb Begin -->
+                   <li>
+                       <a href="index.php">Home</a>
+                   </li>
+                   <li>
+                       Cart
+                   </li>
+               </ul><!-- breadcrumb Finish -->
+               
+           </div><!-- col-md-12 Finish -->
+           
+           <div id="cart" class="col-md-9"><!-- col-md-9 Begin -->
+               
+               <div class="box"><!-- box Begin -->
+                   
+                   <form action="cart.php" method="post" enctype="multipart/form-data"><!-- form Begin -->
+                       
+                       <h1>Shopping Cart</h1>
+                       <p class="text-muted">You currently have 3 item(s) in your cart</p>
+                       
+                       <div class="table-responsive"><!-- table-responsive Begin -->
+                           
+                           <table class="table"><!-- table Begin -->
+                               
+                               <thead><!-- thead Begin -->
+                                   
+                                   <tr><!-- tr Begin -->
+                                       
+                                       <th colspan="2">Product</th>
+                                       <th>Quantity</th>
+                                       <th>Unit Price</th>
+                                       <th colspan="1">Delete</th>
+                                       <th colspan="2">Sub-Total</th>
+                                       
+                                   </tr><!-- tr Finish -->
+                                   
+                               </thead><!-- thead Finish -->
+                               
+                               <tbody><!-- tbody Begin -->
+                                   
+                                   <tr><!-- tr Begin -->
+                                       
+                                                                             <td>
+                                           
+                                           <img class="img-responsive" src="admin/slide_images/slideimages3.jpg" >
+                                           
+                                       </td>
+
+                                       
+                                       <td>
+                                           
+                                           <a href="#">Iphone xr</a>
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                          
+                                           2
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           $50
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           <input type="checkbox" name="remove[]">
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           $100
+                                           
+                                       </td>
+                                       
+                                   </tr><!-- tr Finish -->
+                                   
+                               </tbody><!-- tbody Finish -->
+                               
+                               <tbody><!-- tbody Begin -->
+                                   
+                                   <tr><!-- tr Begin -->
+                                       
+                                       
+                                       
+                                       
+                                   </tr><!-- tr Finish -->
+                                   
+                               </tbody><!-- tbody Finish -->
+                               
+                               
+                               <tfoot><!-- tfoot Begin -->
+                                   
+                                   <tr><!-- tr Begin -->
+                                       
+                                       <th colspan="5">Total</th>
+                                       <th colspan="2">$100</th>
+                                       
+                                   </tr><!-- tr Finish -->
+                                   
+                               </tfoot><!-- tfoot Finish -->
+                               
+                           </table><!-- table Finish -->
+                           
+                       </div><!-- table-responsive Finish -->
+                       
+                       <div class="box-footer"><!-- box-footer Begin -->
+                           
+                           <div class="pull-left"><!-- pull-left Begin -->
+                               
+                               <a href="index.php" class="btn btn-default"><!-- btn btn-default Begin -->
+                                   
+                                   <i class="fa fa-chevron-left"></i> Continue Shopping
+                                   
+                               </a><!-- btn btn-default Finish -->
+                               
+                           </div><!-- pull-left Finish -->
+                           
+                           <div class="pull-right"><!-- pull-right Begin -->
+                               
+                               
+                               <a href="checkout.php" class="btn btn-primary">
+                                   
+                                   Proceed Checkout <i class="fa fa-chevron-right"></i>
+                                   
+                               </a>
+                               
+                           </div><!-- pull-right Finish -->
+                           
+                       </div><!-- box-footer Finish -->
+                       
+                   </form><!-- form Finish -->
+                   
+               </div><!-- box Finish -->
+               
+             
+                            
+                        </div><!-- product same-height Finish -->
+                   </div><!-- col-md-3 col-sm-6 center-responsive Finish -->
+                   
+               </div><!-- #row same-heigh-row Finish -->
+               
+           </div><!-- col-md-9 Finish -->
+           
+        
+           </div><!-- col-md-3 Finish -->
+           
+       </div><!-- container Finish -->
+   </div><!-- #content Finish -->
+   
+   <?php 
+    
+    include("includes/footer.php");
+    
+    ?>
+    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
